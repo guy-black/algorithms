ad0c6f765786c731c1cf552e9cd1b632bc5ac191
guy-black <67166723+guy-black@users.noreply.github.com>
2020-07-16 17:18:53 -0500

    fingers crossed this works!

---
+++ precache-manifest.874b7b1ed4cf449f048521d2994037eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcfeb00e9e81e34d3d35b02a869e061e",
+    "url": "/algorithms/index.html"
+  },
+  {
+    "revision": "e8941b768e9da5b33656",
+    "url": "/algorithms/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "dee162136d4b295a46d6",
+    "url": "/algorithms/static/js/2.0d93ef9a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/algorithms/static/js/2.0d93ef9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8941b768e9da5b33656",
+    "url": "/algorithms/static/js/main.c536dfde.chunk.js"
+  },
+  {
+    "revision": "68c7980b80b5f03f035b",
+    "url": "/algorithms/static/js/runtime-main.317713e5.js"
+  }
+]);
